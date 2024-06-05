c5edb648b13c5888e6f57a7e0853e7be275d1e3f
teoria-kimura <162103512+teoria-kimura@users.noreply.github.com>
2024-06-05 12:39:11 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,14 @@
+// 変数numに0～4までのランダムな整数を代入する
+let num = Math.floor(Math.random() * 30);
+
+console.log(num);
+
+if(num % 15 === 0) {
+  console.log('3と5の倍数です');
+} else if(num % 5 === 0) {
+  console.log('5の倍数です');
+} else if(num % 3 === 0) {
+  console.log('3の倍数です');
+} else {
+  console.log(num);
+}
